5abfce290f2ee5d09cfc9dc63e88799fe1e26281
golaxxx <127908494+golaxxx@users.noreply.github.com>
2024-07-04 21:13:15 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,32 @@
+const caixaprincipal = document.querySelector(".caixaprincipal");
+const caixa_de_perguntas = document.querySelector(".caixa_de_perguntas");
+const caixa_secundaria = document.querySelector(".caixa_alternativas");
+const caixa_de_resultado = document.querySelector(".caixa_de_resultado");
+const textoresultado = document.querySelector(".textoresultado");
+
+const perguntas = [
+    {
+    enunciado: "pergunta1"
+    alternativas:
+    "alternativa 1",
+    "alternativa 2"
+    },
+    {
+        enunciado: "pergunta1"
+        alternativas:
+        "alternativa 1",
+        "alternativa 2"
+        },
+        {
+            enunciado: "pergunta1"
+            alternativas:
+            "alternativa 1",
+            "alternativa 2"
+            },
+            {
+                enunciado: "pergunta1"
+                alternativas:
+                "alternativa 1",
+                "alternativa 2"
+                }
+]
