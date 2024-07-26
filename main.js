d9ef83d673f8ef62b4be74a960dd26f40a77d723
golaxxx <127908494+golaxxx@users.noreply.github.com>
2024-07-25 21:12:02 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,32 +1,92 @@
-const caixaprincipal = document.querySelector(".caixaprincipal");
-const caixa_de_perguntas = document.querySelector(".caixa_de_perguntas");
-const caixa_secundaria = document.querySelector(".caixa_alternativas");
-const caixa_de_resultado = document.querySelector(".caixa_de_resultado");
-const textoresultado = document.querySelector(".textoresultado");
+const caixaPrincipal = document.querySelector(".Caixa-principal");
+const caixaPerguntas = document.querySelector(".Caixa-perguntas");
+const caixaAlternativas =
+document.querySelector(".Caixa-alternativas");
+const caixaResultado = document.querySelector(".Caixa-Resultado");
+const textoResultado = document.querySelector(".Texto-resultado");
 
 const perguntas = [
+{
+    enunciado: "nosso texto",
+    caixaAlternativas: [
     {
-    enunciado: "pergunta1"
-    alternativas:
-    "alternativa 1",
-    "alternativa 2"
+    texto: "Não é um esporte.",
+    afirmacao: "afirmacao"
     },
     {
-        enunciado: "pergunta1"
-        alternativas:
-        "alternativa 1",
-        "alternativa 2"
+    texto: "Sim é um espote.",
+    afirmacao: "afirmacao"
+    }
+    ]
+    },
+    {
+        enunciado: "nosso texto",
+        alternativa: [
+        {
+        texto: "Não é um esporte.",
+        afirmacao: "afirmacao"
         },
         {
-            enunciado: "pergunta1"
-            alternativas:
-            "alternativa 1",
-            "alternativa 2"
+        texto: "Sim é um espote.",
+        afirmacao: "afirmacao"
+        }
+        ]
+        },
+        {
+            enunciado: "nosso texto",
+            alternativa: [
+            {
+            texto: "Não é um esporte.",
+            afirmacao: "afirmacao"
             },
             {
-                enunciado: "pergunta1"
-                alternativas:
-                "alternativa 1",
-                "alternativa 2"
+            texto: "Sim é um espote.",
+            afirmacao: "afirmacao"
+            }
+            ]
+            },
+            {
+                enunciado: "nosso texto",
+                alternativa: [
+                {
+                texto: "Não é um esporte.",
+                afirmacao: "afirmacao"
+                },
+                {
+                texto: "Sim é um espote.",
+                afirmacao: "afirmacao"
                 }
-]
+                ]
+                },
+        ]
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "dasgvdagsdf";
+function mostraPergunta () {
+if(atual >= perguntas. length) {
+mostraResultado();
+return;
+}
+perguntaAtual = perguntas [atual];
+caixaPerguntas. textContent = perguntaAtual. enunciado;
+caixaAlternativas. textContent = " ";
+mostraAlternativas ();
+}
+function mostraAlternativas(){
+for (const alternativa of perguntaAtual. alternativas){
+const botaoAlternativas = document. createElement ("button") ;
+botaoAlternativas. textContent = alternativa.texto;
+botaoAlternativas.addEventListener ("click", () => respostaSelecionada(alternativa))
+caixaAlternativas.appendChild(botaoAlternativas);}
+}
+function respostaSelecionada (opcaoSelecionada){
+const afirmacoes = opcaoselecionada.afirmacoes;
+historiaFinal += afirmacoes +"";
+atual++;
+mostraPergunta();
+}
+function mostraResultado()
+caixaPerguntas. textContent = "jogos ou games são esportivos?"
+textoResultado. textContent = historiaFinal;
+caixaAlternativas.textContent = "";
+mostraPergunta ();
